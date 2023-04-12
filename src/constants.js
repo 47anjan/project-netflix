cccdfcb26c51aeb3e295d245eb54382579609791
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-04-12 14:38:46 +0600

    added .env file

--- src/constants.js
+++ src/constants.js
@@ -9,4 +9,4 @@ export const IMG_PATH_500 = "https://image.tmdb.org/t/p/w500/";
 export const IMG_PATH_300 = "https://image.tmdb.org/t/p/w342/";
 export const IMG_PATH_180 = "https://image.tmdb.org/t/p/w185/";
 
-export const API_KEY = "f043ea02e3806fc663233f74b0323c9a";
+export const API_KEY = import.meta.env.VITE_API_KEY;
