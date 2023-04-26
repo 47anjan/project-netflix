57e4b6026cfe6b798cadda336007e603c9f8d1e9
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-04-26 20:12:10 +0600

    update nav state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   const [searchText, setSearchText] = useState("");
 
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
 
   const handleOnChange = (e) => {
     const value = e.target.value;
